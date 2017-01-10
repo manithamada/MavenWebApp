b700f15807d5552ba0bc662c2e673c242d90fdad
phtaran <differenceN2@yandex.ru>
2017-01-10 21:10:09 +0200

    Move CookieUtils to utils package

--- src/main/java/com/taran/ph/servlets/cookie/AccessCounterServlet.java
+++ src/main/java/com/taran/ph/servlets/cookie/AccessCounterServlet.java
@@ -1,5 +1,7 @@
 package com.taran.ph.servlets.cookie;
 
+import com.taran.ph.utils.CookieUtils;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
--- src/main/java/com/taran/ph/utils/CookieUtils.java
+++ src/main/java/com/taran/ph/utils/CookieUtils.java
@@ -1,4 +1,4 @@
-package com.taran.ph.servlets.cookie;
+package com.taran.ph.utils;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
